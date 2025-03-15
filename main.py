2499601500bfefc6422c7ed90e8506478709956f
yKaayo <caiokaea@gmail.com>
2025-03-15 19:34:27 -0300

    Port Added

--- main.py
+++ main.py
@@ -41,3 +41,7 @@ def get_imagem(id: int):
         return FileResponse(img_url)
     
     return {"erro": "Produto não encontrado"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
